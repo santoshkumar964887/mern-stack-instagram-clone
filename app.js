6a84b2f061507a6c8d59c45c20e777d9570caa93
Santosh Kumar <santoshkumar964887@gmail.com>
2020-09-26 11:25:27 +0530

    making connection with mongoDB

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-import express from 'express';
-const app=express();
-const port=process.env.PORT||5000;
-app.get('/',(req,res)=>{
-    res.status(200).send("Hello world")
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({ path: "./config.env" });
+
+const app = express();
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => console.log(`Data Base Connected${con}`)).catch((error)=>console.log(error));
+const port = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.status(200).send("Hello world");
 });
-app.listen(port,()=>{
-    console.log(`Server is running on port: ${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
 });
