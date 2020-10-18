76b3852cfebcd43e3d58d0d215513a5caa3a68e8
Santosh Kumar <santoshkumar964887@gmail.com>
2020-10-18 07:09:33 +0530

    creating new user

--- router/userAuthRoute.js
+++ router/userAuthRoute.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import userModel from '../model/userModel.js';
 const userRouter=express.Router();
 
 userRouter.get('/',(req,res)=>{
     console.log('santosh')
    res.send("hello")
 });
-userRouter.post('/signup',(req,res)=>{
+userRouter.post('/signup',async(req,res)=>{
     const {name,email,password}=req.body;
     if(!name || !email || !password ){
         return res.status(422).json({
@@ -13,9 +14,13 @@ userRouter.post('/signup',(req,res)=>{
             message:"every fields must have value"
         })
     }
+   const data= await userModel.create({ name,email,password});
     res.status(201).json({
         status:"success",
-        message:"new user has been created"
+        message:"new user has been created",
+        data:{
+            data
+        }
     })
 
 })
