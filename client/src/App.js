f3b4f080a23eb2436c2b1e9340f578401630b4e1
Santosh Kumar <santoshkumar964887@gmail.com>
2020-10-20 18:37:52 +0530

    seting routes, header,diffrent pages like home,profile,login,singup

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,23 @@
-import React, { Component } from 'react';
-import axios from "axios";
-import './App.css';
-class App extends Component {
-
-  constructor(){
-    super();
-    this.state = {
-    search:"",
-    error: "",
-    posts: [],
-    };
-
-  }
-
-
- render() {
-    
-    return (
-      <>
-       santosh
-    </>
-    );
-  }
-}
+import React from "react";
+import {Switch,Route} from 'react-router-dom';
+import Header from "./components/Header";
+import SignupPage from "./pages/Signup";
+import HomePage from "./pages/Home";
+import LoginPage from "./pages/Login";
+import ProfilePage from "./pages/Profile";
+import "./App.css";
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/signup' component={SignupPage}/>
+        <Route exact path='/login' component={LoginPage}/>
+        <Route exact path='/home' component={HomePage}/>
+        <Route exact path='/profile' component={ProfilePage}/>
+      </Switch>
+    </div>
+  );
+};
 
 export default App;
