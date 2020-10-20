35e082f9c0bf4f23d878d54a5bf7ddb87e1a6269
Santosh Kumar <santoshkumar964887@gmail.com>
2020-10-20 16:56:48 +0530

    setup client side

--- controller/userPostController.js
+++ controller/userPostController.js
@@ -1,44 +1,44 @@
-const userPostModel=require('../model/postModel');
+const userPostModel = require("../model/postModel");
 //creating new post
-exports.userPostController = async(req, res) => {
+exports.userPostController = async (req, res) => {
   const { title, body } = req.body;
   if (!title || !body) {
     return res
       .status(422)
       .json({ status: "fail", message: "please fill all the fields" });
-  };
-  req.user.password=undefined;
-  try{
- const data=await userPostModel.create({
-     title,
-     body,
-     postedBy:req.user
- });
- res.status(201).json({status:"success",data})
-}catch(err){
-    res.status(500)
-    .json({ status: "fail", message: "internal server error" });
-}
-
-
-
+  }
+  req.user.password = undefined;
+  try {
+    const data = await userPostModel.create({
+      title,
+      body,
+      postedBy: req.user,
+    });
+    res.status(201).json({ status: "success", data });
+  } catch (err) {
+    res.status(500).json({ status: "fail", message: "internal server error" });
+  }
 };
 // fetching all post
-exports.getAllPosts= async(req,res)=>{
-  try{
-   const data=await userPostModel.find().populate("postedBy","_id name");
-   return res.status(200).json({status:"success",data});
-  } catch(err){
-    res.status(500).json({status:"fail",message:err})
+exports.getAllPosts = async (req, res) => {
+  try {
+    const data = await userPostModel.find().populate("postedBy", "_id name");
+    return res.status(200).json({ status: "success", "number of items": data.length, data });
+  } catch (err) {
+    res.status(500).json({ status: "fail", message: err });
   }
-}
-exports.getMyPost= async(req,res)=>{
-  Post.find({postedBy:req.user._id})
-    .populate("PostedBy","_id name")
-    .then(mypost=>{
-        res.json({mypost})
+};
+exports.getMyPost = async (req, res) => {
+  try{
+  Post.find({ postedBy: req.user._id })
+    .populate("PostedBy", "_id name")
+    .then((data) => {
+      return res.status(200).json({ status: "success", "number of items": data.length, data });
     })
-    .catch(err=>{
-        console.log(err)
-    })
-}
+    .catch((err) => {
+      res.status(500).json({ status: "fail", message: "internal server error" });
+    });
+  } catch(err){
+    res.status(500).json({ status: "fail", message: "internal server error" });
+  }
+};
