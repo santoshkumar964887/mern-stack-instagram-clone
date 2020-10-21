38ce8ec0d587635a10c0cebc568c5dc9f2a83c25
Santosh Kumar <santoshkumar964887@gmail.com>
2020-10-21 12:22:46 +0530

    designing login and singup component

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import {Link} from 'react-router-dom';
 export default function Login() {
-    return (
-        <div>
-            Login
-        </div>
-    )
+  return (
+    <div className="cardContainer">
+      <div className="card input-field">
+      <h2 style={{textAlign:"center"}}>Instagram</h2>
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <br/>
+        <button
+          class="btn waves-effect #1e88e5 blue darken-1"
+          type="submit"
+          name="action"
+        >
+           Login
+        </button>
+        <br/>
+        <h6>If you don't have account <Link to="/signup" ><span style={{color:"#1996fc",fontSize:14,fontWeight:"600"}}>Register here</span></Link> </h6>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
-const Signup=()=>{
-    return(
-        <div>Signup</div>
-    );
-};
-export default Signup;
+import React from "react";
+import {Link} from 'react-router-dom';
+export default function Signup() {
+  return (
+    <div className="cardContainer">
+      <div className="card input-field">
+      <h2 style={{textAlign:"center"}}>Instagram</h2>
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <br/>
+        <button
+          class="btn waves-effect #1e88e5 blue darken-1"
+          type="submit"
+          name="action"
+        >
+           Register
+        </button>
+        <br/>
+        <h6>If you have account <Link to="/login" ><span style={{color:"#1996fc",fontSize:14,fontWeight:"600"}}>Login here</span></Link> </h6>
+      </div>
+    </div>
+  );
+}
