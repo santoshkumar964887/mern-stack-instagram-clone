282485b6cd32f6a6444d16dcf87c838638e1b8f7
Santosh Kumar <santoshkumar964887@gmail.com>
2020-11-29 21:51:17 +0530

    implementing like and dislike functionlity in backend

--- router/userPostsRoute.js
+++ router/userPostsRoute.js
@@ -8,5 +8,6 @@ postRoute
   .post(userRequire, controller.userPostController)
   .get(userRequire, controller.getAllPosts);
 postRoute.route("/mypost").get(userRequire, controller.getMyPost);
-
+postRoute.route("/like").get(controller.likePost);
+postRoute.route("/dislike").get(controller.DisLikePost);
 module.exports = postRoute;
